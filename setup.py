1eaaee41bfa73cd912368179d0b6806d9bcc3973
Léo Flaventin Hauchecorne <lf.hauchecorne@fairvision.fr>
2020-08-25 01:25:53 +0200

    f dependencies

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     test_suite=None,
     include_package_data=True,
     zip_safe=False,
-    install_requires=['click>=0.7', 'appdirs>=1.4.4'],
+    install_requires=['click>=0.7', 'appdirs>=1.4.4', 'tempiny>=1.0'],
     extras_require=None,
     entry_points={
       'console_scripts':[
